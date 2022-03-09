8394cc693229d75e776ab6a40eb88f1b38c329c8
PJDong <1115957667@qq.com>
2022-03-09 23:07:00 +0800

    update

--- runs.py
+++ runs.py
@@ -8,7 +8,7 @@ import torch.nn.init as init
 import numpy as np
 from torch.autograd import Variable
 from PIL import Image
-
+from torchvision import transforms
 
 class BasicConv(nn.Module):
 
@@ -70,9 +70,29 @@ class BasicRFB(nn.Module):
 class RFBFeature(nn.Module):
     def __init__(self):
         super(RFBFeature, self).__init__()
-        self.Norm = BasicRFB(1024, 1024, scale = 1.0, visual=2)
+        layers = [
+            nn.Conv2d(3, 64, kernel_size=3),
+            # nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3),
+            # nn.ReLU(),
+            
+            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=1),
+            # nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=1),
+            # nn.ReLU(), # 8
+            
+            nn.Conv2d(128, 256, kernel_size=3),
+            # nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3),
+            # nn.ReLU(), # 13
+        ]        
+        self.stem = nn.Sequential(*layers)    
+        self.Norm = BasicRFB(256, 256, scale = 1.0, visual=2)
         
     def forward(self, x):
+        print(x.shape)
+        x = self.stem(x)
         s = self.Norm(x)
         return s
 
@@ -90,24 +110,47 @@ def weights_init(m):
             m.state_dict()[key][...] = 0
 
 net.Norm.apply(weights_init) #initial
+input_shape = [32, 32, 3]
 
+imgt = Image.open(r"./dd.jpg", mode="r")
+imgt = imgt.resize((input_shape[0], input_shape[1]), Image.ANTIALIAS)
+trans = transforms.Compose([transforms.ToTensor(),
+			 transforms.Resize(32),
+			 transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))]) 
 
-x = Variable(torch.zeros(1,1024,19,19), requires_grad=True) #input
+x = trans(imgt)
+
+x = x.unsqueeze(0)
+    
+# imgt = tile_pil_image(imgt, tile_factor=0, shade=True)
+
+x = Variable(x, requires_grad=True) #input
 out = net(x) #output
 
-Zero_grad = torch.Tensor(1,1024,19,19).zero_() #Zero_grad
+Zero_grad = torch.Tensor(1,256,10,10).zero_() #Zero_grad
 
-Zero_grad[0][512][9][9] = 1.0 #set the middle pixel to 1.0
+Zero_grad[0][...][5][5] = 1 #set the middle pixel to 1.0
 
 out.backward(Zero_grad) #backward
 
 z = x.grad.data.cpu().numpy() #get input graident
-z = np.mean(z,axis=1) #calculate mean by channels
+
+print(z)
+z = np.sum(np.abs(z), axis=1)
+
+# z = np.mean(z,axis=1) #calculate mean by channels
+# # z = np.array(z).mean(0).
+# z /= z.max() 
+# z += (np.abs(z) > 0) * 0.2 
+
 
 #convert to 0-255
 z = z * 255 / np.max(z)
 z = np.uint8(z)
 z = z[0,:,:]
 
-img = Image.fromarray(z) #convert to image
-img.show()
+# img = Image.fromarray(z) #convert to image
+import matplotlib.pyplot as plt 
+
+plt.imshow(z)
+plt.savefig("out.png", dpi=200)
